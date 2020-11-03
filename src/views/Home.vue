ba21539f3796e7b0d149494e2044fd5930e9ac5a
“Aria <a.c.nikzad@gmail.com>
2020-11-02 17:38:20 -0800

    added car create

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <p>Add car year here:<input type="text" v-model="newCarYear"></p>
+    <p>Add car make here<input type="text" v-model="newCarMake"></p>
+    <p>Add car model here<input type="text" v-model="newCarModel"></p>
+    <p>Add car color here<input type="text" v-model="newCarColor"></p>
+    <p>Add car mileage here<input type="text" v-model="newCarMileage"></p>
+    <p>Add car lot here<input type="text" v-model="newCarLot_id"></p>
+
+    <button v-on:click="addCar()">Create Car</button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      cars: [],
+      newCarYear:"",
+      newCarMake: "",
+      newCarModel: "",
+      newCarColor: "",
+      newCarMileage:"",
+      newCarLot_id:"",
+      currentCar: {}
+
+    };
+  },
+  created: function() {
+    axios.get(`/api/cars`).then(response => {
+      this.cars =  response.data;
+      console.log(response);
+    });
+  },
+  methods: {
+    addCar: function() {
+      console.log("fucntion made");
+      var params = {
+        year: this.newCarYear,
+        make: this.newCarMake,
+        model: this.newCarModel,
+        color: this.newCarColor,
+        mileage: this.newCarMileage,
+        lot_id: this.newCarLot_id
+      };
+
+      axios.post("/api/cars", params).then(response => {
+        console.log(response.data);
+        this.cars.push(response.data);
+        this.newCarYear = "";
+        this.newCarMake = "";
+        this.newCarModel = "";
+        this.newCarColor = "";
+        this.newCarMileage = "";
+      });
+    }
   }
-}
+};
 </script>
