0e79880467dc06fda1621fa639dac0e4fa201f00
“Aria <a.c.nikzad@gmail.com>
2020-11-03 18:00:23 -0800

    added update function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,13 @@
     <p>Add car lot here<input type="text" v-model="newCarLot_id"></p>
     <button v-on:click="addCar()">Create Car</button>
     <br>
+
+ <!--     <h3>Search for Car make here:</h3><input type="text" v-model="searchTerm" list="makes">
+     <datalist id="makes">
+      <option v-for="car in cars">{{car.make}}</option>
+    </datalist> -->
+     <br>
+
     <div class="box" v-for="car in cars">
       <ul class="carList">
         <li>id: {{car.id}}</li>
@@ -19,8 +26,23 @@
         <li>mileage: {{car.mileage}}</li>
         <li>lot id: {{car.lot_id}}</li>
       </ul>
+      <button v-on:click="toggleInfo(car)">More Info</button>
       <button v-on:click="destroyCar(car)">Delete Car</button>
+
+      <div v-if="currentCar === car">
+        <h3>Update Car</h3>
+        <p>Year:</p><input type="text" v-model="car.year">
+        <p>Make:</p><input type="text" v-model="car.make">
+        <p>Model:</p><input type="text" v-model="car.model">
+        <p>Color:</p><input type="text" v-model="car.color">
+        <p>Mileage:</p><input type="text" v-model="car.mileage">
+        <p>Lot:</p><input type="text" v-model="car.lot_id">
+        <br>
+        <button v-on:click="updateCar(car)">Update Car</button>
+      </div>
     </div>
+
+
   </div>
 </template>
 
@@ -40,7 +62,9 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from 'vue2-filters';
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function() {
     return {
       message: "Welcome to Vue.js!",
@@ -81,6 +105,7 @@ export default {
         this.newCarModel = "";
         this.newCarColor = "";
         this.newCarMileage = "";
+        this.newCarLot_id = "";
       });
     },
 
@@ -90,6 +115,39 @@ export default {
         var index = this.cars.indexOf(theCar);
         console.log(index);
         this.cars.splice(index, 1);
+      });
+    },
+
+    toggleInfo: function(theCar) {
+      if (this.currentCar === theCar) {
+        this.currentCar = null;
+      } else { 
+        this.currentCar = theCar;
+      }
+      console.log('toggling info...')
+      ;
+    },
+
+    updateCar: function(theCar) {
+      console.log('Updating the car...');
+      console.log(theCar);
+
+      var params = {
+        year: theCar.year,
+        make: theCar.make,
+        model: theCar.model,
+        color: theCar.color,
+        mileage: theCar.mileage,
+        lot_id: theCar.lot_id
+      };
+
+      axios.patch(`/api/cars/${theCar.id}`, params).then(response => {
+        console.log(response.data);
+        theCar.year = response.data.year;
+        theCar.make = response.data.make;
+        theCar.model = response.data.model;
+        theCar.color = response.data.color;
+        theCar.lot_id = response.data.lot_id;
       });
     }
   }
